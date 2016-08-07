58627bf76ec574435f6c95b91d6f4462b41822b8
Victor Laznik <victor.laznik@gmail.com>
2016-08-07 18:00:42 -0400

    branch up and running

---
+++ app/assets/javascripts/gifts.js
@@ -0,0 +1,8 @@
+// var html = ['<a class="delete" href="#">Delete</a>']
+// $('a.delete').on('click', function(event) {
+//   $(this).parents('li').remove()
+// });
+
+$(window).ready(function(){
+  console.log("hi")
+})
