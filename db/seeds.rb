95f9fa42cbbb8d06a4b777b48c9c871301238ca5
Adam Hill <enjoilif3@gmail.com>
2016-08-07 12:31:32 -0400

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.find_or_create_by([{first_name: "Arin", last_name: "Damson", email: "ecocopter@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Dirk", last_name: "Peter", email: "tankingcandle@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Stark", last_name: "Hamilton", email: "ballerz@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Vitas", last_name: "Stroganov", email: "sevenelements@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Surge", last_name: "Machampion", email: "kepler@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Jack", last_name: "Ull", email: "kackles@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Flash", last_name: "Groudon", email: "grounded@example.co", password:BCrypt::Password("password")}])
+user = User.find_or_create_by([{first_name: "Ash", last_name: "Sketchem", email: "sketchymon@example.co", password:BCrypt::Password("password")})
+user = User.find_or_create_by([{first_name: "Jonothan", last_name: "Kage", email: "totalcombat@example.co", password:BCrypt::Password("password")})
+user = User.find_or_create_by([{first_name: "Damian", last_name: "Bayne", email: "ghulies@example.co", password:BCrypt::Password("password")}])
