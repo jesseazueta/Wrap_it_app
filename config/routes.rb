1b3dae19929928a3b0dbdb239875fccfb6fb3386
Jesus Barrenechea <jesseazueta@gmail.com>
2016-08-07 11:25:15 -0400

    added gift routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :friendships
+  resources :gifts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
