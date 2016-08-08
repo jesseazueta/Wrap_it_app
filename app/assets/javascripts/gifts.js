22a614490dc83bd9088eb46f9d0c531e4974a9ec
Victor Laznik <victor.laznik@gmail.com>
2016-08-08 11:03:58 -0400

    added some Jquery, and one jQuery incomplete

--- app/assets/javascripts/gifts.js
+++ app/assets/javascripts/gifts.js
@@ -1,8 +1,8 @@
-// var html = ['<a class="delete" href="#">Delete</a>']
-// $('a.delete').on('click', function(event) {
-//   $(this).parents('li').remove()
-// });
+var html = ['<a class="delete" href="#">Delete</a>', '<a class="wrap-it" href="*">Wrap-It!</a>']
+$('a.delete').on('click', function(event) {
+  $(this).parents('li').remove()
+});
 
-$(window).ready(function(){
-  console.log("hi")
+$('a.wrap-it').on('click', function(event){
+  $(this).parents('li').
 })
