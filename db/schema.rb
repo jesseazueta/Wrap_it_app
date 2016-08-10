c8b1d14af301a3a7cff60bd986249ea82dc2cad9
Jesus Barrenechea <jesseazueta@gmail.com>
2016-08-10 10:17:22 -0400

    updated search page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160809135432) do
-
+ActiveRecord::Schema.define(version: 20160809160206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +27,8 @@ ActiveRecord::Schema.define(version: 20160809135432) do
     t.string   "model"
     t.integer  "price"
     t.string   "category"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.integer  "user_id"
     t.string   "store"
     t.string   "weblink"
@@ -39,7 +37,6 @@ ActiveRecord::Schema.define(version: 20160809135432) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "purchased",          default: false
-
   end
 
   create_table "users", force: :cascade do |t|
