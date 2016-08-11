85d7815846e8295b4fec4407ccb2c1e129d12bfa
Adam Hill <enjoilif3@gmail.com>
2016-08-11 19:47:02 -0400

    Replace friend button with text on ajax post success

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,13 @@
 $(document).on('turbolinks:load', function() {
 	$('.add-friend').click(function(){
+		var button = this
 		var newFriendId = $(this).attr('data-user-id');
 		var currentUser = $('#back-to-profile').attr('data-current-user-id');
-		$.post('/friendships', { friendship: { user_id: currentUser, friend_id: newFriendId } });
+		$.post('/friendships', { friendship: { user_id: currentUser, friend_id: newFriendId } },
+			function(){
+				console.log('yay');
+				$(button).replaceWith('Friends!');
+			}
+		);
 	})
 });
